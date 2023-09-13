87e8c6299a24becf01b98b191ad48233d60d9c51
Signor-Koala <jeffrey.samuel@protonmail.com>
2023-09-13 16:06:53 +0530

    refactoring since goose bullied me

--- B01-SRV/database/db_handler.js
+++ B01-SRV/database/db_handler.js
@@ -1,14 +1,10 @@
 const userModel = require('./schemas/user.js');
 
 async function addUser(user) {
-  const newUser = new userModel({
-    username: user.username_field,
-    firstname: user.name1_field,
-    lastname: user.name2_field,
-    pronouns: user.pronouns_field,
-    age: user.age_field,
-  });
+  const newUser = new userModel(user);
   await newUser.save();
 }
 
-module.exports = addUser;
+module.exports = {
+  addUser,
+};
--- B01-SRV/routes/routes.js
+++ B01-SRV/routes/routes.js
@@ -4,25 +4,25 @@ const router = express.Router();
 const { body, matchedData, validationResult } = require('express-validator');
 const dbh = require('../database/db_handler.js');
 
-const pages = path.join(__dirname, '../assets');
+const pages = path.join(__dirname, '../static');
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.post('/submit-form',
-  body('username_field').notEmpty().matches(/[a-z_]+/).escape(),
-  body('name1_field').notEmpty().matches(/[a-zA-Z]+/),
-  body('name2_field').notEmpty().matches(/[a-zA-Z]+/),
-  body('pronouns_field').notEmpty().matches(/[a-zA-Z]+\/[a-zA-Z]+/),
-  body('age_field').notEmpty().matches(/\d+/),
-  (req, res) => {
+  body('username').notEmpty().matches(/[a-z_]+/).escape(),
+  body('name1').notEmpty().matches(/[a-zA-Z]+/),
+  body('name2').notEmpty().matches(/[a-zA-Z]+/),
+  body('pronouns').notEmpty().matches(/[a-zA-Z]+\/[a-zA-Z]+/),
+  body('age').notEmpty().matches(/\d+/),
+  async (req, res) => {
 
   const result = validationResult(req);
 
   if (result.isEmpty()) {
     const data = matchedData(req);
     console.log('Validation success');
-    dbh(data);
+    await dbh.addUser(data);
     res.status(200);
     res.send('Success!');
   }
